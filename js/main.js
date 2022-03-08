17596314c71a9072bb0973fde8a89cd53161aa9b
Johnson1255 <johnson.cheny.col@upb.edu.co>
2022-03-07 22:00:40 -0500

    Commit 9

--- js/main.js
+++ js/main.js
@@ -6,14 +6,47 @@ const total_pokemons = 1126;
 window.onload = inicio;
 
 function aleatorio(min, max) {
-    return Math,floor(Math,random() * (max - min + 1) + min)
+    return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
 function inicio()
 {
-    contenedor = document,getElementById("vitrina");
+    contenedor = document.getElementById("vitrina");
     window.addEventListener("click",pintarVitrina);
 }
 
+function pintarVitrina(evento){
+    contenedor.innerHTML = "";
+    traerDatos(aleatorio(1,total_pokemons));
+    traerDatos(aleatorio(1,total_pokemons));
+    traerDatos(aleatorio(1,total_pokemons));
+    traerDatos(aleatorio(1,total_pokemons));
+    traerDatos(aleatorio(1,total_pokemons));
+}
 
+function traerDatos(id){
+    fetch (`https://pokeapi.co/api/v2/pokemon/${id}`)
+    .then(response => response.json())
+    .then(function(data){
+        let nombre = data.name;
+        let url = data.sprites.ther.dream.world.front_default;
+        if(nombre && url)
+        {
+            imprimirTarjeta(nombre,url);
+        }
+    });
+}
+
+function imprimirTarjeta(nombre,url)
+{
+    let template = `<div class="tarjeta">
+    <img src="${url}" alt = "" >
+    <br>
+    <label for="">
+    ${nombre}
+    </label> <br>
+    <a href="">Ver Más...</a>
+    </div>`;
+    contenedor.innerHTML += template;
+}
 
